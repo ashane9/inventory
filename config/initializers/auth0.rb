05d0780968e3238aa163a108549cc703bec0e843
Andrew Shane <ashane9@gmail.com>
2021-04-11 18:41:43 -0400

    moved credential read from yml

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,13 +1,9 @@
-puts "before config load"
-AUTH0_CONFIG = Rails.application.config_for(:auth0)
-puts "after config load"
-puts Rails.application.credentials.auth0[:auth0_client_id]
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    AUTH0_CONFIG['auth0_client_id'],
-    AUTH0_CONFIG['auth0_client_secret'],
-    AUTH0_CONFIG['auth0_domain'],
+    Rails.application.credentials.auth0[:auth0_client_id],
+    Rails.application.credentials.auth0[:auth0_client_secret],
+    Rails.application.credentials.auth0[:auth0_domain],
     callback_path: '/auth/auth0/callback',
     authorize_params: {
       scope: 'openid profile'
