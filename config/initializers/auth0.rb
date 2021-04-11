bd3961ebce224b0b613e0dc798450c147ed2afdd
Andrew Shane <ashane9@gmail.com>
2021-04-11 19:18:49 -0400

    working on cred

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    Rails.application.credentials.auth0[:auth0_client_id],
-    Rails.application.credentials.auth0[:auth0_client_secret],
-    Rails.application.credentials.auth0[:auth0_domain],
+    Rails.application.credentials[Rails.env.to_sym][:auth0][:auth0_client_id],
+    Rails.application.credentials[Rails.env.to_sym][:auth0][:auth0_client_secret],
+    Rails.application.credentials[Rails.env.to_sym][:auth0][:auth0_domain],
     callback_path: '/auth/auth0/callback',
     authorize_params: {
       scope: 'openid profile'
