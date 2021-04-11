45eb01f8c6612f9079513de25d4240bb4b5cf621
Andrew Shane <ashane9@gmail.com>
2021-04-11 18:34:23 -0400

    cleaning up auth files

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,5 +1,7 @@
+puts "before config load"
 AUTH0_CONFIG = Rails.application.config_for(:auth0)
-
+puts "after config load"
+puts Rails.application.credentials.auth0[:auth0_client_id]
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
