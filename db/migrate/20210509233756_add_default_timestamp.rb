60d4cfac46a82b8f7ca2866455e8361df2c30e83
ashane9 <ashane9@gmail.com>
2021-05-12 20:58:26 -0400

    removing unneeded migration

--- db/migrate/20210509233756_add_default_timestamp.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultTimestamp < ActiveRecord::Migration[6.0]
-  def change
-    change_column :authentications_autographs, :created_at, :timestamps, default: 'CURRENT_TIMESTAMP'
-    change_column :authentications_autographs, :updated_at, :timestamps, default: 'CURRENT_TIMESTAMP'
-  end
-end
