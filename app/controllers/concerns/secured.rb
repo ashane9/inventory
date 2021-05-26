11609aa30a8f8b9a63865aaac15a1cd260afb815
Andrew Shane <ashane9@gmail.com>
2021-05-25 22:10:28 -0400

    added except show for secured helper

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -2,7 +2,7 @@ module Secured
   extend ActiveSupport::Concern
 
   included do
-    before_action :logged_in_using_omniauth?
+    before_action :logged_in_using_omniauth?, except: 'show'
   end
 
   def logged_in_using_omniauth?
