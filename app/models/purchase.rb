e5679cde2da2909990d36afd887bf2bb6a9a7c4f
Andrew Shane <ashane9@gmail.com>
2021-06-01 23:32:58 -0400

    added stats on total cost

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,4 +4,22 @@ class Purchase < ApplicationRecord
   has_many :autographs
   accepts_nested_attributes_for :autographs, reject_if: proc { |att| att['name'].blank? }
   belongs_to :purchase_type, optional: true
+
+  def self.owner(owned_by)
+    where(owned_by: owned_by)
+  end
+
+  def self.sum_total_cost
+    sum(:total_cost)  
+  end
+
+  def self.item_purchases(owned_by)
+    where(id: Item.where(owned_by: owned_by).select(:purchase_id).distinct)
+  end
+
+  def self.autograph_purchases(owned_by)
+    where(id: Autograph.where(owned_by: owned_by).select(:purchase_id).distinct)
+  end
+
+
 end
